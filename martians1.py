1b164ed575189ce70d432d240071a8c9b78cedfe
aigerim28 <150646854+aigerim28@users.noreply.github.com>
2023-11-26 19:46:29 +0600

    Update martians1.py
    The new function for generating random weights for cargos was added.

--- martians1.py
+++ martians1.py
@@ -1,3 +1,17 @@
+
+import random
+
+def generate_weights(total_weight):
+    weights = []
+    cargo1 = random.randint(1, total_weight - 2)
+    cargo2 = random.randint(1, total_weight - 1)
+    cargo3 = total_weight - cargo1 - cargo2
+    
+    weights.append(cargo1)
+    weights.append(cargo2)
+    weights.append(cargo3)
+    return weights
+
 # This is the main function of our program. It gets 3 positions from user
 # until it gets the correct ones with cargos with total weight 713 kg.
 
